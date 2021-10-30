d867a61146c82228ef54515d09985d6068ec83cf
haedoang <haedoang@gmail.com>
2021-10-30 14:57:19 +0900

    feat : chapter 1.6
    - Weakness of Singleton Pattern
    - Definition of Bean's Scope

--- vol1/chapter1/src/main/java/user/dao/UserDao.java
+++ vol1/chapter1/src/main/java/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package user.dao;
 
 import common.db.ConnectionMaker;
-import common.db.DConnectionMaker;
 import user.domain.User;
 
 import java.sql.*;
@@ -11,17 +10,20 @@ import java.sql.*;
  * fileName : UserDao
  * author : haedoang
  * date : 2021/10/30
- * description : 관심사의 분리를 적용한 UserDao
+ * description : 관심사의 분리를 적용한 UserDao, Singleton 적용하기
  */
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private ConnectionMaker connectionMaker; //가능. 읽기전용이기 떄문
+    private Connection c; // 매번 바뀌는 인스턴스 변수
+    private User user;    // 매번 바뀌는 인스턴스 변수
+    //개별적으로 바뀌는 변수는 로컬 변수를 활용해야 멀티스레드 환경에서 문제가 발생하지 않는다.
 
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        this.c = connectionMaker.makeConnection();
 
         String query = new StringBuilder("insert into users(id, name, password)")
                 .append(" values (?,?,?)").toString();
@@ -39,7 +41,7 @@ public class UserDao {
 
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        this.c = connectionMaker.makeConnection();
 
         String query = new StringBuilder("select * from users where id = ?").toString();
         PreparedStatement ps = c.prepareStatement(query);
@@ -47,7 +49,7 @@ public class UserDao {
 
         ResultSet rs = ps.executeQuery();
         rs.next();
-        User user = new User();
+        this.user = new User();
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
@@ -56,7 +58,7 @@ public class UserDao {
         ps.close();
         c.close();
 
-        return user;
+        return this.user;
 
     }
 
